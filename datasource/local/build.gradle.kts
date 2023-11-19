ddfcbdd3dabd99c9bad51577623287b9ac2e378e
Alexandru Vlad <alexandru.vlad1208@gmail.com>
2023-11-19 02:15:00 +0200

    Add unit tests for list viewmodel nad rearrange code

---
+++ datasource/local/build.gradle.kts
@@ -0,0 +1,22 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.ksp)
+    id("com.deskbird.android.lib")
+    id("com.deskbird.android.di")
+}
+
+android {
+    namespace = "com.deskbird.datasource.local"
+}
+
+dependencies {
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    implementation(project(":domain:api"))
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
---
+++ domain/api/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.deskbird.android.lib")
+    id("com.deskbird.android.di")
+}
+
+android {
+    namespace = "com.deskbird.domain.api"
+}
