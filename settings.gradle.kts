23f7ddec8db8a06d43a32bd8ddcd6ffd6dffe358
Alexandru Vlad <alexandru.vlad1208@gmail.com>
2023-11-22 15:35:59 +0200

    Automatically include new modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,19 +21,8 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "Brewzard"
-include(":app")
-include(":domain:api")
-include(":domain:impl")
-include(":datasource:remote")
-include(":datasource:local")
-include(":ui:strings")
-include(":ui:design-system")
-include(":ui:util")
-include(":test:data")
-include(":test:util")
-include(":feature:breweries-list:domain")
-include(":feature:breweries-list:ui")
-include(":feature:breweries-favorites:domain")
-include(":feature:breweries-favorites:ui")
-include(":feature:breweries-details:domain")
-include(":feature:breweries-details:ui")
+include(fileTree(rootProject.projectDir)
+    .filter { it.name == "build.gradle.kts" }
+    .map { it.parent.replace(rootProject.projectDir.path, "").replace("/", ":") }
+    .filter { !it.contains("buildLogic") || !it.contains("buildSrc") }
+)
